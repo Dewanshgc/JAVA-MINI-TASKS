45b15216ed03baace11e5323c335ada9effa683b
DEWANSH <129524187+Dewanshgc@users.noreply.github.com>
2026-04-03 22:43:58 +0000

    update

---
+++ TASK/03-04-2026/jaggedarray.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class twodarray{
+	
+}
+class dk{
+	public static void main(String[]args){
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter the length of the array : ");
+		int b=sc.nextInt();
+		int a[]=new int[b];
+		for(int i=0;i<b;i++){
+			System.out.print("Enter the index value at "+i+" : ");
+			a[i]=sc.nextInt();
+		}
+		System.out.print("{ ");
+		for(int i=0;i<b;i++){
+			System.out.print(a[i]+"                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+		}
+		System.out.print("}");
+		
+	}
+}
