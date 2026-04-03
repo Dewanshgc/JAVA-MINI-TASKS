5630a2d9edf774f8a42852dbec2aa931226c9ffa
DEWANSH <129524187+Dewanshgc@users.noreply.github.com>
2026-04-03 22:44:48 +0000

    update

---
+++ TASK/01-04-2026/practice.java
@@ -0,0 +1,10 @@
+class dk{
+	public static void main(String[]args){
+		int a[]={5, 8, 9, 56, 7, 56, 5, 1, 5, 5};
+		for(int i=0;i<a.length;i++){
+			for(int j=0;j<a.length;j++){
+				
+			}
+		}
+	}
+}
---
+++ TASK/01-04-2026/variable.java
@@ -0,0 +1,11 @@
+class dk{
+	public static void main(String[]args){
+		int a=10;
+		void method(){
+			int b=20;
+			System.out.print(b);
+		}
+		static int c=30;
+		System.out.print();
+	}
+}
